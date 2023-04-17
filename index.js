711ab254bdad4c08bd3396139e9e4dea741819b1
Saul Ceja <saulceja08@gmail.com>
2023-04-16 22:06:17 -0700

    Complete Lab

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+function returnFirstTwoDrivers(){
+    return drivers.slice(0,2);
+}
+  
+function returnLastTwoDrivers() {
+    return drivers.slice(-2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers]
+
+function createFareMultiplier(integer) {
+    return function(fare) {
+      return fare * integer;
+    }
+  }
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(arrayOfDrivers, driverFunction) {
+    return driverFunction(arrayOfDrivers);
+  }
+  
+function selectDifferentDrivers(drivers, driverFunction) {
+    if (driverFunction === returnFirstTwoDrivers) {
+      return returnFirstTwoDrivers(drivers);
+    } else if (driverFunction === returnLastTwoDrivers) {
+      return returnLastTwoDrivers(drivers);
+    }
+}
